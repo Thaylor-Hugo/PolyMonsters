fb44d1e4c9be219d0d4b6f1401ad65d17ccda192
antonio <antspjr@outlook.com>
2023-07-15 17:08:16 -0300

    esqueci de colocar a classe MediumState no commit anterior

---
+++ src/state/MediumState.java
@@ -0,0 +1,10 @@
+package state;
+
+public class MediumState implements DifficultyState  {
+
+    @Override
+    public void play() {
+        System.out.println("Jogando em modo medio");
+    }
+    
+}
